c663697a1c43cd269da922e2d5a39637121c231f
sandrap02 <sandrap410@gmail.com>
2018-06-04 21:28:41 +0100

    DB connet

--- DataBase.class.php
+++ DataBase.class.php
@@ -9,4 +9,85 @@
 
  class DataBase {
 
+    const CONNECT_TIMEOUT = 5; // in seconds
+
+    /**
+     * Variavel caminho BD
+     * 
+     * @var string
+     */
+    private $dsn;
+
+    /**
+     * Username BD
+     * 
+     * @var string
+     */
+    private $username;
+
+    /**
+     * Password BD
+     * 
+     * @var string
+     */
+    private $password;
+
+    /**
+     * Ligaçao BD
+     * 
+     * @var object
+     */
+    private $connection;
+
+    /**
+     * Undocumented variable
+     *
+     * @var array
+     */
+    private $pdoOptions;
+
+    /**
+     * Construtor da classe
+     * 
+     * @param string $dsn Host da ligaçao
+     * @param string $username Username da BD
+     * @param string $password Password da BD
+     * 
+     * @return void
+     * 
+     */
+    public function __construct($dsn, $username, $password){
+        $this->dsn = $dsn;
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->pdoOptions = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_TIMEOUT => self::CONNECT_TIMEOUT
+        ];
+
+        $this->makeConection();
+    }
+
+    /**
+     * Criar ligação
+     *
+     * @return void
+     */
+    protected function makeConection(){
+        $this->connection = new PDO($this->dsn, $this->username, $this->password, $this->pdoOptions);
+
+        return $this->connection;
+    }
+
+    /**
+     * Verifica a ligação à BD
+     *
+     * @return boolean
+     */
+    public function isConnected(){
+        
+        return (boolean) $this->connection;
+    }
  }
